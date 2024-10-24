f2dd5c529e22dc0eb84458d4b95c3948f9777c3c
Sergio Teacher <Sergio Teacher@MINIWIN>
2024-10-24 16:36:00 +0200

    Primero ...

---
+++ src/block_de_notasbeans/Block_de_NotasBeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package block_de_notasbeans;
+
+/**
+ *
+ * @author Sergio Teacher
+ */
+public class Block_de_NotasBeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Iniciando ...");
+    }
+    
+}
